f6f04b7e447189f298812ca103bf6807bdef98aa
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-08-10 16:17:19 +0530

    add comments for simple

--- simple.py
+++ simple.py
@@ -3,20 +3,33 @@ from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+
 load_dotenv()
 
+
+# Initialize the LLM
 model = ChatOpenAI()
 
 
+# Create a prompt template with a dynamic {topic} variable
 template = PromptTemplate(
     template="Generate 5 interesting facts about {topic}",
-    input_variables=['topic'] 
+    input_variables=['topic']
 )
 
+
+# Converts the LLM's AIMessage response into a plain string
 parser = StrOutputParser()
 
+
+# LCEL chain:
+# PromptTemplate → LLM → StrOutputParser
 chain = template | model | parser
 
-reponse = chain.invoke({'topic': 'Bugs'})
 
-print(reponse)
+# Pass the value for {topic} and execute the chain
+response = chain.invoke({'topic': 'Bugs'})
+
+
+# Print the final parsed string
+print(response)
